c41ae0f5d279ff8fa0c5087a72a3b859ea03b6e5
Jonathan PHILIPPE <pretrine@hotmail.fr>
2017-07-23 15:02:22 +0200

    Fixes env variables

--- auth_server/src/routes/auth.js
+++ auth_server/src/routes/auth.js
@@ -1,24 +1,24 @@
 import fetch from 'node-fetch'
 
-function _oauth_token(code, redirect_uri) {
-  return fetch('https://api.intra.42.fr/oauth/token', {
+async function _oauth_token(code, redirect_uri) {
+  const res = await fetch('https://api.intra.42.fr/oauth/token', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       'code': code,
-      'client_id': 'CLIENT_ID',
-      'client_secret': 'CLIENT_SECRET',
+      'client_id': CLIENT_ID,
+      'client_secret': CLIENT_SECRET,
       'redirect_uri': redirect_uri,
       'grant_type': 'authorization_code'
     })
-  }).then(res => {
-    return res.json()
   })
+
+  return res.json()
 }
 
-export default (req, res) => {
+export default async (req, res) => {
   const code = req.query.code
   const redirect_uri = req.query.redirect_uri || 'com.paulvarry.intra42://oauth2redirect'
 
@@ -29,14 +29,14 @@ export default (req, res) => {
     })
   }
 
-  _oauth_token(code, redirect_uri).then(json => {
-    if (json.error) {
-      res.status(401).json({
-        error: 401,
-        message: json.error_description
-      })
-    } else {
-      res.status(200).json(json)
-    }
-  })
+  const json = await _oauth_token(code, redirect_uri)
+
+  if (json.error) {
+    res.status(401).json({
+      error: 401,
+      message: json.error_description
+    })
+  } else {
+    res.status(200).json(json)
+  }
 }
