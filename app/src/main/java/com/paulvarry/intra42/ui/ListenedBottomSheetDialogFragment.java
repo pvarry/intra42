f743b3e35c3ef3ba43fd26e40a095731994aa3cb
Paul Varry <paulvarry@gmail.com>
2018-04-29 12:07:34 +0200

    remove useless state listener on bottom sheet

--- app/src/main/java/com/paulvarry/intra42/ui/ListenedBottomSheetDialogFragment.java
+++ app/src/main/java/com/paulvarry/intra42/ui/ListenedBottomSheetDialogFragment.java
@@ -2,16 +2,9 @@ package com.paulvarry.intra42.ui;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomSheetBehavior;
-import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.BottomSheetDialogFragment;
-import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.FrameLayout;
 
 import com.paulvarry.intra42.R;
 
@@ -37,42 +30,12 @@ public class ListenedBottomSheetDialogFragment extends BottomSheetDialogFragment
         super.onStart();
         Dialog dialog = getDialog();
         if (dialog != null) {
-            int width = getContext().getResources().getDimensionPixelSize(R.dimen.bottom_sheet_width);
+            int width = requireContext().getResources().getDimensionPixelSize(R.dimen.bottom_sheet_width);
 
             Window window = dialog.getWindow();
             if (window != null) {
                 window.setLayout(width > 0 ? width : ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
             }
         }
-    }
-
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {
-                BottomSheetDialog showedDialog = (BottomSheetDialog) dialog;
-                FrameLayout bottomSheet = showedDialog.findViewById(android.support.design.R.id.design_bottom_sheet);
-                if (bottomSheet == null)
-                    return;
-                BottomSheetBehavior behaviour = BottomSheetBehavior.from(bottomSheet);
-                behaviour.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-                    @Override
-                    public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                        if (newState == BottomSheetBehavior.STATE_EXPANDED) {
-                            Log.d("ex", "ex");
-                        }
-                    }
-
-                    @Override
-                    public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-
-                    }
-                });
-            }
-        });
-        return dialog;
     }
 }
