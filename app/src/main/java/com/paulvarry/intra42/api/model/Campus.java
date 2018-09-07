8ab4064afd59893c69b4220b2be6ca487a399e6e
Paul Varry <paulvarry@gmail.com>
2018-09-07 14:23:40 +0200

    update Campus model

--- app/src/main/java/com/paulvarry/intra42/api/model/Campus.java
+++ app/src/main/java/com/paulvarry/intra42/api/model/Campus.java
@@ -1,14 +1,15 @@
 package com.paulvarry.intra42.api.model;
 
-import androidx.annotation.Nullable;
 import com.google.gson.annotations.SerializedName;
 import com.paulvarry.intra42.api.ApiService;
 import com.paulvarry.intra42.utils.Pagination;
-import retrofit2.Response;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.Nullable;
+import retrofit2.Response;
 
 public class Campus {
 
@@ -17,6 +18,14 @@ public class Campus {
     private static final String API_TIME_ZONE = "time_zone";
     private static final String API_LANGUAGE = "language";
     private static final String API_USERS_COUNT = "users_count";
+    private static final String API_VOGSPHERE_ID = "vogsphere_id";
+    private static final String API_COUNTRY = "country";
+    private static final String API_ADDRESS = "address";
+    private static final String API_ZIP = "zip";
+    private static final String API_CITY = "city";
+    private static final String API_WEBSITE = "website";
+    private static final String API_FACEBOOK = "facebook";
+    private static final String API_TWITTER = "twitter";
 
     @SerializedName(API_ID)
     public int id;
@@ -28,6 +37,22 @@ public class Campus {
     public int userCount;
     @SerializedName(API_LANGUAGE)
     public Language language;
+    @SerializedName(API_VOGSPHERE_ID)
+    public int vogsphereId;
+    @SerializedName(API_COUNTRY)
+    public String country;
+    @SerializedName(API_ADDRESS)
+    public String address;
+    @SerializedName(API_ZIP)
+    public String zip;
+    @SerializedName(API_CITY)
+    public String city;
+    @SerializedName(API_WEBSITE)
+    public String website;
+    @SerializedName(API_FACEBOOK)
+    public String facebook;
+    @SerializedName(API_TWITTER)
+    public String twitter;
 
     public static List<String> getStrings(List<Campus> campusList) {
         List<String> l = new ArrayList<>();
