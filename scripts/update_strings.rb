02a90e8571cb0b1d91a4d325efdc8c4afc2ee970
Paul Varry <paulvarry@gmail.com>
2019-08-19 21:15:27 +0200

    Update strings and project data

--- scripts/update_strings.rb
+++ scripts/update_strings.rb
@@ -20,17 +20,21 @@ conn = Net::HTTP.new('api.poeditor.com', 443)
 conn.use_ssl = true
 conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-puts "Initialize language list"
+puts 'Initialize language list'
 req = Net::HTTP::Post.new('/v2/languages/list')
 req.body = URI.encode_www_form({ api_token: POEDITOR_API_KEY, id: POEDITOR_PROJET_ID })
 res = conn.request(req)
 json = JSON.parse(res.body)
 
-languages = json['result']['languages'].reject { |language| language['translations'].zero? } #ignore empty translation
+languages = json['result']['languages'].reject { |language| language['translations'].zero? } # Ignore empty translation
                                        .map { |language| language['code'] }
-languages.each do |language|
+languages.each do |data|
+  language, country = data.split('-')
+  local = country ? "#{language}-r#{country.upcase}" : language
+  filename = 'values'
+  filename += "-#{local}" if local != 'en' # Use english as default
 
-  puts "Update language: #{language}"
+  puts "Update language: #{local}"
   req = Net::HTTP::Post.new('/v2/projects/export')
   req.body = URI.encode_www_form({ api_token: POEDITOR_API_KEY, id: POEDITOR_PROJET_ID, language: language, type: 'android_strings', filters: 'translated' })
   res = conn.request(req)
@@ -38,7 +42,7 @@ languages.each do |language|
 
   req = Net::HTTP::Get.new(json['result']['url'])
   res = conn.request(req)
-  language_path = File.join(OUTPUT_PATH, "values#{language == 'en' ? '' : "-#{language}"}")
+  language_path = File.join(OUTPUT_PATH, filename)
     
   FileUtils.mkdir_p(language_path)
   File.write(File.join(language_path, 'strings.xml'), res.body)
