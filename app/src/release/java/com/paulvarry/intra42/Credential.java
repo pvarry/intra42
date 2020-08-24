670677014e2441e50027feb7c238096dd430c825
Paul Varry <paulvarry@gmail.com>
2020-08-24 21:14:15 +0200

    [VERSION] 0.12.6

---
+++ app/src/release/java/com/paulvarry/intra42/Credential.java
@@ -0,0 +1,10 @@
+package com.paulvarry.intra42;
+
+public class Credential {
+
+    public static final String UID = "61b857fd1949918d723b33c61a18d2e24323af7824b60a2b09b5ae36909ef17e";
+    public static final String SECRET = null;
+    public static final String API_OAUTH_REDIRECT = "com.paulvarry.intra42://oauth2redirect";
+    public static final String SCOPE = "public projects profile elearning";
+
+}
